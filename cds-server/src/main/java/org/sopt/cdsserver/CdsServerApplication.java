97189db1808db8143cdab1ade5cbf1998e93bf9d
sohyundoh <lyny.doh@gmail.com>
2023-11-16 11:42:22 +0900

    [chore] 프로젝트 초기화

---
+++ cds-server/src/main/java/org/sopt/cdsserver/CdsServerApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.cdsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdsServerApplication.class, args);
+	}
+
+}
