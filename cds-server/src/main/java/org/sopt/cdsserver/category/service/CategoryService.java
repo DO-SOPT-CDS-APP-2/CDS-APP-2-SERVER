3fd031934ef8173c53bbf384f82fd18f548497a0
sohyundoh <lyny.doh@gmail.com>
2023-11-19 15:16:17 +0900

    #7 [feat] 내부 로직 작성

---
+++ cds-server/src/main/java/org/sopt/cdsserver/category/service/CategoryService.java
@@ -0,0 +1,26 @@
+package org.sopt.cdsserver.category.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.cdsserver.category.domain.Category;
+import org.sopt.cdsserver.common.exception.enums.ErrorType;
+import org.sopt.cdsserver.common.exception.model.NotFoundException;
+import org.sopt.cdsserver.product.controller.dto.response.ProductListResponse;
+import org.sopt.cdsserver.category.repository.CategoryJpaRepository;
+import org.sopt.cdsserver.product.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryJpaRepository categoryJpaRepository;
+    private final ProductService productService;
+
+    public List<ProductListResponse> getCategoryList(final int categoryId) {
+        Category category = categoryJpaRepository.findById(Long.valueOf(categoryId)).orElseThrow(
+                () -> new NotFoundException(ErrorType.CATEGORY_NOT_FOUND_EXCEPTION)
+        );
+        return productService.getProductListByCategory(category);
+    }
+}
