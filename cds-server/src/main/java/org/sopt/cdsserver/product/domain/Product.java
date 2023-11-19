f10dd176c4b75a355d897a4ee41cb15e204ded82
sohyundoh <lyny.doh@gmail.com>
2023-11-19 14:57:06 +0900

    #7 [feat] Product 내부 가격 정보 임베드 클래스로 전환

--- cds-server/src/main/java/org/sopt/cdsserver/product/domain/Product.java
+++ cds-server/src/main/java/org/sopt/cdsserver/product/domain/Product.java
@@ -1,12 +1,17 @@
 package org.sopt.cdsserver.product.domain;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
 import org.sopt.cdsserver.category.domain.Category;
 
+@Getter
 @Entity
 public class Product {
     @Id
@@ -15,11 +20,14 @@ public class Product {
 
     private String title;
 
+    private String imageUrl;
+
+
+    @Enumerated(value = EnumType.STRING)
     private Brand brand; //enum
 
-    private int price;
-
-    private int discount;
+    @Embedded
+    private PriceInfo priceInfo;
 
     private String details;
 
